af379ba45603adb854c7d009eb6aed289987d769
NR5P <orangev8zcar@gmail.com>
2021-01-17 16:11:22 -0600

    succesffully reading tag id

--- RFID.py
+++ RFID.py
@@ -8,7 +8,7 @@ import chardet
 
 class RFID:
     def __init__(self):
-        self.debug = False
+        self.debug = True
         self.opcode = ''
         self.ser = serial.Serial(
             port='/dev/ttyUSB0',
@@ -24,45 +24,48 @@ class RFID:
         configBlob = [0x00, 0x00, 0x01, 0x22, 0x00, 0x00, 0x05, 0x07, 0x22, 0x10, 0x00, 0x1B, 0x03, 0xE8, 0x01, 0xFF]
         self.sendMessage(TMR_SR_OPCODE_MULTI_PROTOCOL_TAG_OP, configBlob)
 
-    def printByteArray(self, tag):
-        txt = ""
-        amtToPrint = int.from_bytes(tag[1],"little") + 5
-        if amtToPrint > MAX_MSG_SIZE:
-            amtToPrint = MAX_MSG_SIZE
+    def printMessageArray(self, msg) -> None:
+        if self.debug == True:
+            amtToPrint = msg[1] + 5
+            if amtToPrint > MAX_MSG_SIZE:
+                amtToPrint = MAX_MSG_SIZE
+            
+            printMsg = ""
+            for i in range(amtToPrint):
+                printMsg += " ["
+                if msg[i] < 0x10:
+                    printMsg += "0"
+                printMsg += hex(msg[i])[2:]
+                printMsg += "]"
+            print(printMsg)
 
-        for i in tag:
-            if int.from_bytes(i,"little") < 0x10:
-                txt += i
-            else:
-                txt += i
-
-        print("[ " + txt + " ]")
-
-    def receiveOneTag(self):
-        msg = []
-        spot = 0
-        message_length = MAX_MSG_SIZE - 1
-        while spot < message_length:
-            if self.ser.inWaiting() > 0:
-                msg.append(self.ser.read(size=1))
-                if spot == 1:
-                    message_length = int.from_bytes(msg[1],"little") + 7
-                spot+=1
-                spot %= MAX_MSG_SIZE
-        return msg
-
-    def readTagEPC(epc, epcLength, timeOut):
+    def readTagEPC(self) -> str:
         bank = 0x01
         address = 0x02
-        return readData(bank, address, epc, epcLength, timeOut)    
+        return self.readData(bank, address, 3)    
 
-    def readData(bank, address, epc, epcLength, timeOut):
-        data = bytearray
+    def readData(self, bank, address, timeOut):
+        data = bytearray()
         data.append(timeOut >> 8 & 0xFF)
         data.append(timeOut & 0xFF)
         data.append(bank)
 
-        data.append(address >> (8 * (3-0)) & 0xFF)
+        for i in range(4):
+            data.append(address >> (8 * (3-0)) & 0xFF)
+        data.append(0x00)
+        if (bank == 0x03):
+            data[7] = 0x00
+        
+        receivedMsg = self.sendMessage(TMR_SR_OPCODE_READ_TAG_DATA, data)
+
+        returnArray = []
+        if receivedMsg[0] == ALL_GOOD:
+            status = ((receivedMsg[3] << 8) & 0xFFFF) | receivedMsg[4]
+            if status == 0x0000:
+                responseLength = receivedMsg[1]
+                for i in range(responseLength):
+                    returnArray[i] = receivedMsg[i + 5]
+                return returnArray
 
     def disableReadFilter(self):
         self.setReaderConfiguration(0x0C, 0x00)
@@ -97,17 +100,20 @@ class RFID:
     def getRate(self) -> int:
         pass
 
-    def sendMessage(self, opcode: int, data: List[int], timeout: int = COMMAND_TIME_OUT, waitforresponse: bool = True) -> None:
+    def sendMessage(self, opcode: int, data: List[int], timeout: int = COMMAND_TIME_OUT, waitforresponse: bool = True) -> List:
         msg = bytearray()
         msg.append(len(data))
         msg.append(opcode)
         for i in data:
             msg.append(i)
-        self.sendCommand(timeout, waitforresponse, msg)
+        return self.sendCommand(timeout, waitforresponse, msg)
 
-    def sendCommand(self, timeout: int, waitforresponse: bool, msg: bytearray) -> None:
-        #self.opcode = msg[1] # to see if response from module has same opcode
+    def sendCommand(self, timeout: int, waitforresponse: bool, msg: bytearray) -> List:
+        self.ser.reset_input_buffer() # clear anything in buffer
+        # self.opcode = msg[1] to see if response from module has same opcode
         msg.insert(0,0xFF) # universal header at beginnning
+        msgLength = msg[1]
+        opcode = msg[2]
 
         # attach crc
         crc = self.calculateCRC(msg)
@@ -117,6 +123,51 @@ class RFID:
             #printMessage(msg)
         self.ser.write(msg)
 
+        # wait for response with timeout
+        startTime = time.time()
+        while self.ser.inWaiting() < 1:
+            if (time.time() - startTime) > timeout:
+                print("NO RESPONSE FROM MODULE")
+                msg[0] = ERROR_COMMAND_RESPONSE_TIMEOUT
+                return
+
+        msgLength = MAX_MSG_SIZE - 1
+        spot = 0
+        receiveArray = []
+        while spot < msgLength:
+            if (time.time() - startTime) > timeout:
+                print("NO RESPONSE FROM MODULE")
+                receiveArray.append(ERROR_COMMAND_RESPONSE_TIMEOUT)
+                return
+            if self.ser.inWaiting() > 0:
+                receiveArray.append(int.from_bytes(self.ser.read(size=1),"little"))
+                if spot == 1:
+                    msgLength = receiveArray[1] + 7
+                spot += 1
+                spot %= MAX_MSG_SIZE
+
+        if self.debug == True:
+            print("response: ")
+            self.printMessageArray(receiveArray)
+
+        crc = self.calculateCRC(receiveArray[1:-3])
+        if (receiveArray[msgLength - 2] != (crc >> 8)) or (receiveArray[msgLength - 1] != (crc & 0xFF)):
+            receiveArray[0] = ERROR_CORRUPT_RESPONSE
+            if (self.debug == True):
+                print("CORRUPT RESPONSE")
+                return
+
+        # If crc is ok, check that opcode matches (did we get a response to the command we sent or a different one?)
+        if (receiveArray[2] != opcode):
+            receiveArray[0] = ERROR_WRONG_OPCODE_RESPONSE
+            if (self.debug == True):
+                print("WRONG OPCODE RESPONSE")
+                return
+
+        # If everything is ok, load all ok into msg array
+        receiveArray[0] = ALL_GOOD
+        return receiveArray
+
     def calculateCRC(self, buf):
         crc = 0xFFFF
         for i in range(1,len(buf)):
@@ -124,14 +175,3 @@ class RFID:
             crc = (((crc << 4) & 0xFFFF) | (buf[i] & 0x0F)) ^ crctable[crc >> 12]
 
         return crc
-
-    def receiveMessage(self) -> List[str]:
-        i = 0
-        msg = [None] * MAX_MSG_SIZE
-        while self.uart.any() > 0:
-            msg[i] = self.uart.read(1).decode("utf-8")
-            i = i + 1
-
-    def sendToUART(self, msg: List[int]) -> None:
-        for i in msg:
-            uart.write(i)
