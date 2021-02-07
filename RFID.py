e016e719e190a3f9d0acbea52b1576a90d5abbfc
NR5P <orangev8zcar@gmail.com>
2021-02-07 08:31:16 -0600

    library for using serial port

--- RFID.py
+++ RFID.py
@@ -1,18 +1,135 @@
 from constants import *
 from typing import Tuple, List
-from ctypes import sizeof
-from machine import uart
+import serial
+import time
+import keyboard
 
 class RFID:
     def __init__(self):
-        self.debug = False
-        self.baudRate = 9600
+        self.debug = True
         self.opcode = ''
-        self.uart = UART(1, self.baudRate)
-        self.uart.init(baudRate=9600,bits=8,parity=None,stop=1,rx=12,tx=14)
+        self.ser = serial.Serial(
+            port='/dev/ttyUSB0',
+            baudrate=115200,
+        )
 
-    def setBaud(self, baudRate: int) -> None:
-        pass
+    def startReading(self, rfOnTime, rfOffTime):
+        self.ser.reset_input_buffer()
+        self.disableReadFilter()
+
+        configBlob = [0x00, 0x00, 0x01, 0x22, 0x00, 0x00, 0x05, 0x0b, 0x22, 0x10, 0x05, 0x1B] 
+        configBlob.append(rfOnTime >> 8 & 0xFF)
+        configBlob.append(rfOnTime & 0xFF)
+        configBlob.append(rfOffTime >> 8 & 0xFF)
+        configBlob.append(rfOffTime & 0xFF)
+        configBlob.append(0)
+        configBlob.append(87)
+        configBlob.append(1)
+        configBlob.append(0)
+
+        self.sendMessage(TMR_SR_OPCODE_MULTI_PROTOCOL_TAG_OP, configBlob, timeout=None)
+
+    def getSignalLevelDB(self, msgArray) -> int:
+        if len(msgArray) > 13 and msgArray[12] != 0:
+            return msgArray[12] - 256
+
+    def getEpcTagNumber(self, msgArray) -> str:
+        epcTagNum = ""
+        if len(msgArray) > 19:
+            epcLength = msgArray[20] / 8
+            for i in range(22,len(msgArray)-4):
+                if msgArray[i] < 0x10:
+                    epcTagNum += "0"
+                epcTagNum += hex(msgArray[i])[2:]
+        return epcTagNum
+
+    def setAntennaPort(self):
+        configBlob = [0x01, 0x01]
+        self.sendMessage(TMR_SR_OPCODE_SET_ANTENNA_PORT, configBlob)
+
+    # Sets the protocol of the module
+    # Currently only GEN2 has been tested and supported but others are listed here for reference
+    # and possible future support
+    def setTagProtocol(self, protocol = TMR_TAG_PROTOCOL_GEN2):
+        data = []
+        data.append(0) # Opcode expects 16-bits
+        data.append(protocol)
+
+        self.sendMessage(TMR_SR_OPCODE_SET_TAG_PROTOCOL, data)
+
+    def printMessageArray(self, msg) -> None:
+        #print(self.getSignalLevelDB(msg))
+        print(self.getEpcTagNumber(msg))
+        if self.debug == True:
+            amtToPrint = msg[1] + 5
+            if amtToPrint > MAX_MSG_SIZE:
+                amtToPrint = MAX_MSG_SIZE
+            
+            printMsg = ""
+            for i in range(amtToPrint):
+                printMsg += " ["
+                if msg[i] < 0x10:
+                    printMsg += "0"
+                printMsg += hex(msg[i])[2:]
+                printMsg += "]"
+            print(printMsg)
+
+    def readTagEPC(self) -> str:
+        bank = 0x01
+        address = 0x02
+        return self.readData(bank, address, 3)    
+
+    #Get the version number from the module
+    def getVersion(self):
+        self.sendMessage(TMR_SR_OPCODE_VERSION, [])
+
+    def getPowerMode(self):
+        self.sendMessage(TMR_SR_OPCODE_GET_POWER_MODE, [])
+
+    def setRegion(self, region):
+        data = bytearray()
+        data.append(region)
+        self.sendMessage(TMR_SR_OPCODE_SET_REGION, data)
+
+    def readData(self, bank, address, timeOut):
+        data = bytearray()
+        data.append(timeOut >> 8 & 0xFF)
+        data.append(timeOut & 0xFF)
+        data.append(bank)
+
+        for i in range(4):
+            data.append(address >> (8 * (3-0)) & 0xFF)
+        data.append(0x00)
+        if (bank == 0x03):
+            data[7] = 0x00
+        
+        receivedMsg = self.sendMessage(TMR_SR_OPCODE_READ_TAG_DATA, data)
+
+        returnArray = []
+        if receivedMsg[0] == ALL_GOOD:
+            status = ((receivedMsg[3] << 8) & 0xFFFF) | receivedMsg[4]
+            if status == 0x0000:
+                responseLength = receivedMsg[1]
+                for i in range(responseLength):
+                    returnArray.append(receivedMsg[i + 5])
+                return returnArray
+
+    def disableReadFilter(self):
+        self.setReaderConfiguration(0x0C, 0x00)
+
+    def setReaderConfiguration(self, option1, option2):
+        data = []
+        data.append(1)
+        data.append(option1)
+        data.append(option2)
+        self.sendMessage(TMR_SR_OPCODE_SET_READER_OPTIONAL_PARAMS, data)
+
+    def setBaudRate(self, baudRate: int) -> None:
+        data = bytearray()
+        for i in range(2):
+            data.append(0xFF & (baudRate >> (8 * (2 - 1 - i))))
+
+        self.sendMessage(TMR_SR_OPCODE_SET_BAUD_RATE, data, waitforresponse=False)
 
     def readTag(self) -> Tuple[str, int]:
         """
@@ -26,63 +143,114 @@ class RFID:
 
     def stopReading(self) -> None:
         configBlob = [0x00, 0x00, 0x02]
-        sendMessage(TMR_SR_OPCODE_MULTI_PROTOCOL_TAG_OP, configBlob, waitforresponse=false) #Do not wait for response
+        self.sendMessage(TMR_SR_OPCODE_MULTI_PROTOCOL_TAG_OP, configBlob, waitforresponse=False) #Do not wait for response
 
-    def setOutputPower(self, powerOut: int) -> None:
-        pass
+    # maximum read power is 2700 for 27db
+    def setReadPower(self, powerSetting: int) -> None:
+        if powerSetting > 32767:
+            powerSetting = 2700
+        if powerSetting < -32768:
+            powerSetting = 0
 
-    def getRate(self) -> int:
-        pass
+        data = bytearray()
+        for i in range(2):
+            data.append(0xFF & (powerSetting >> (8 * (2 - 1 - i))))
+        self.sendMessage(TMR_SR_OPCODE_SET_READ_TX_POWER, data)
 
-    def sendMessage(self, opcode: int, data: List[int], timeout: int = COMMAND_TIME_OUT, waitforresponse: bool = True) -> List[str]:
-        #TODO: use bytearray
-        msg = [None] * MAX_MSG_SIZE
-        msg[0] = 0xFF #universal header
-        msg[1] = len(data)
-        msg[2] = opcode
-        for i in range(len(data)):
-            msg[3 + i] = data[i]
-        self.sendCommand(timeout, waitforresponse, msg)
-        if waitforresponse == True:
-            receivedMsg = self.receiveMessage()
-            #msgLength = receivedMsg[1] + 7
-            #crc = self.calculateCRC(receivedMsg[1], msgLength - 3); #Remove header, remove 2 crc bytes
-            return receivedMsg
-        else:
-            return []
+    def getReadPower(self):
+        data = bytearray()
+        data.append(0x00)
+        self.sendMessage(TMR_SR_OPCODE_GET_READ_TX_POWER, data)
 
-    def sendCommand(self, timeout: int, waitforresponse: bool, msg: List[int]) -> None:
+    def getWritePower(self):
+        data = bytearray()
+        data.append(0x00)
+        self.sendMessage(TMR_SR_OPCODE_GET_WRITE_TX_POWER, data)
+
+    def sendMessage(self, opcode: int, data: List[int], timeout: int = COMMAND_TIME_OUT, waitforresponse: bool = True) -> List:
+        msg = bytearray()
+        msg.append(len(data))
+        msg.append(opcode)
+        for i in data:
+            msg.append(i)
+        return self.sendCommand(timeout, waitforresponse, msg)
+
+    def checkTimeOut(self, startTime, timeout):
+        if timeout != None and (time.time() - startTime) > timeout:
+            return True 
+        return False
+
+    def checkKeyPress(self):
+        if keyboard.is_pressed("q"):
+            self.stopReading()
+            return True
+
+    def sendCommand(self, timeout: int, waitforresponse: bool, msg: bytearray) -> List:
+        msg.insert(0,0xFF) # universal header at beginnning
         msgLength = msg[1]
-        self.opcode = msg[2] # to see if response from module has same opcode
+        opcode = msg[2]
 
         # attach crc
-        crc = self.calculateCRC(msg[1], msgLength + 2)
-        msg[msgLength + 3] = crc >> 8
-        msg[msgLength + 4] = crc & 0xFF
-        if self.debug == True:
-            printMessage(msg)
+        crc = self.calculateCRC(msg)
+        msg.append(crc >> 8)
+        msg.append(crc & 0xFF)
+        self.ser.reset_input_buffer() # clear anything in buffer
+        self.ser.write(msg)
 
-        self.sendToUART(msg)
+        # wait for response with timeout
+        startTime = time.time()
+        while self.ser.inWaiting() < 1:
+            if self.checkTimeOut(startTime, timeout):
+                print("NO RESPONSE FROM MODULE")
+                msg[0] = ERROR_COMMAND_RESPONSE_TIMEOUT
+                return
 
 
-    def calculateCRC(self, buf: int, len: int) -> int:
+        while True:
+            msgLength = MAX_MSG_SIZE - 1
+            spot = 0
+            receiveArray = []
+            if self.checkKeyPress() == True:
+                return
+            while spot < msgLength:
+                if self.checkTimeOut(startTime, timeout):
+                    receiveArray.append(ERROR_COMMAND_RESPONSE_TIMEOUT)
+                    return receiveArray
+                if self.ser.inWaiting() > 0:
+                    receiveArray.append(int.from_bytes(self.ser.read(size=1),"little"))
+                    if spot == 1:
+                        msgLength = receiveArray[1] + 7
+                    spot += 1
+            if self.debug == True:
+                print("response: ")
+                self.printMessageArray(receiveArray)
+
+            # check crc for corrupted response
+            crc = self.calculateCRC(receiveArray[:-2]) # remove the header(0xff) and 2 crc bytes
+            if (receiveArray[msgLength - 2] != (crc >> 8)) or (receiveArray[msgLength - 1] != (crc & 0xFF)):
+                receiveArray[0] = ERROR_CORRUPT_RESPONSE
+                if (self.debug == True):
+                    print("CORRUPT RESPONSE")
+                    return receiveArray
+
+            # check that opcode matches (did we get a response to the command we sent or a different one?)
+            if (receiveArray[2] != opcode):
+                receiveArray[0] = ERROR_WRONG_OPCODE_RESPONSE
+                if (self.debug == True and timeout != None):
+                    print("WRONG OPCODE RESPONSE")
+                    return receiveArray
+
+            # If everything is ok, load all ok into msg array
+            receiveArray[0] = ALL_GOOD
+            if timeout != None:
+                return receiveArray
+
+    def calculateCRC(self, buf):
         crc = 0xFFFF
-        for i in range(len):
-            crc = ((crc << 4) | (buf[i] >> 4)) ^ crctable[crc >> 12]
-            crc = ((crc << 4) | (buf[i] & 0x0F)) ^ crctable[crc >> 12]
+        for i in range(1,len(buf)):
+            crc = (((crc << 4) & 0xFFFF) | (buf[i] >> 4)) ^ crctable[crc >> 12]
+            crc = (((crc << 4) & 0xFFFF) | (buf[i] & 0x0F)) ^ crctable[crc >> 12]
 
         return crc
 
-    def printMessage(self, msg: List[int]) -> None:
-        print(msg)
 
-    def receiveMessage(self) -> List[str]:
-        i = 0
-        msg = [None] * MAX_MSG_SIZE
-        while self.uart.any() > 0:
-            msg[i] = self.uart.read(1).decode("utf-8")
-            i = i + 1
-
-    def sendToUART(self, msg: List[int]) -> None:
-        for i in msg:
-            uart.write(i)
---
+++ test.py
@@ -0,0 +1,24 @@
+import time
+from constants import *
+
+############configuration settings########################
+rfOnTime = 500
+rfOffTime = 500
+readPower = 2700
+##########################################################
+
+from RFID import RFID
+rf = RFID()
+rf.setBaudRate(115200)
+rf.setTagProtocol(TMR_TAG_PROTOCOL_GEN2)
+rf.setAntennaPort()
+rf.setRegion(REGION_EU3)
+rf.setReadPower(readPower)
+
+#rf.readTagEPC()
+#time.sleep(1)
+#rf.getReadPower()
+rf.startReading(rfOnTime,rfOffTime)
+#rf.stopReading()
+
+#rf.getVersion()
