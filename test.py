026f1a1e9c6ea2e5d9c66ce15bcd50d6dbc49617
NR5P <orangev8zcar@gmail.com>
2021-03-07 20:43:35 -0600

    works

--- test.py
+++ test.py
@@ -5,14 +5,12 @@ from constants import *
 rfOnTime = 500
 rfOffTime = 500
 readPower = 2700
-#baudrate = 115200 # 115200 is default for m6e nano
 baudrate = 115200 # 115200 is default for m6e nano
 ##########################################################
 
 from RFID import RFID
 rf = RFID(baudrate)
-rf.setBaudRate(baudrate)
-print("set baud rate")
+##rf.setBaudRate(baudrate)
 rf.setTagProtocol(TMR_TAG_PROTOCOL_GEN2)
 rf.setAntennaPort()
 rf.setRegion(REGION_EU3)
